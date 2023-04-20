bedc11cc99bb99371d81ee006c0d76aca1ff0674
Graham Taylor <gktaylor@ucsc.edu>
2023-04-20 12:30:01 -0700

    comit

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -19,27 +19,35 @@ class Rocket extends Phaser.GameObjects.Sprite {
             if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
                 this.x += this.moveSpeed;
             }
-            if(Phaser.Input.Keyboard.JustDown(keyF)){
+            
+        }
+        if(Phaser.Input.Keyboard.JustDown(keyF)){
 
-                this.isFiring = true;
-            }
+            this.isFiring = true;
+        }
 
-
-            if(this.isFiring && this.y >= borderUISize * 3 + borderPadding){
-                this.y -= this.moveSpeed;
-
-            }
-
-            if(this.y <= borderUISize * 3 + borderPadding){
-                this.isFiring = false;
-                this.y = game.config.height - borderUISize - borderPadding;
-
-            }
-
+        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding){
+            this.y -= this.moveSpeed;
 
         }
 
+        if(this.y <= borderUISize * 3 + borderPadding){
+            this.isFiring = false;
+            this.y = game.config.height - borderUISize - borderPadding;
 
+        }
+
+           
+
+
+
+
+
+    }
+
+    reset(){
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
 
 
     }
--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -18,5 +18,9 @@ class Spaceship extends Phaser.GameObjects.Sprite {
 
     }
 
+    reset() {
+
+        this.x = game.config.width;
+    }
 
 }
